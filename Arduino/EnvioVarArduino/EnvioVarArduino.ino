57f6bb2d93905ba6f61e9bde7c4705ff4391c80f
Lprigara <lprigara@gmail.com>
2015-06-16 11:43:31 +0100

    Implementación del envío de variables mediante dispositivo Bluetooth

--- Arduino/EnvioVarArduino/EnvioVarArduino.ino
+++ Arduino/EnvioVarArduino/EnvioVarArduino.ino
@@ -1,19 +1,25 @@
 #include <PinChangeInt.h>
+#include <SoftwareSerial.h>
 #include <Event.h>
 #include <Timer.h>
 #include <eHealth.h>
 
 Timer timer; 
+SoftwareSerial Bluetooth(2, 3); // RX | TX
 int cont =0;
 char delimitador = '+';
 char finPaquete = '~';
 
 void setup()
    { 
+     Bluetooth.flush();
+     delay(500);
      Serial.begin(9600);
+     Bluetooth.begin(9600);
      eHealth.initPulsioximeter();
-     timer.every(7000, enviarVariables)
+     timer.every(7000, enviarVariables);
      PCintPort::attachInterrupt(6, readPulsioximeter, RISING);
+     delay(100);
    }
 
 void loop(){  
@@ -22,18 +28,19 @@ void loop(){
    
 void enviarVariables()
 {
-    Serial.print("#"); 
-    Serial.print(delimitador);
-    Serial.print(eHealth.getBPM());
-    Serial.print(delimitador);  
-    Serial.print(eHealth.getOxygenSaturation());
-    Serial.print(delimitador);
-    Serial.print(eHealth.getTemperature());
-    Serial.print(delimitador); 
-    Serial.print(eHealth.getSkinConductance());
-    Serial.print(delimitador); 
-    Serial.print(eHealth.getSkinResistance());
-    Serial.print(finPaquete);
+    if(Bluetooth.isListening()){
+    Bluetooth.print("#"); 
+    Bluetooth.print(delimitador);
+    Bluetooth.print(eHealth.getBPM());
+    Bluetooth.print(delimitador);  
+    Bluetooth.print(eHealth.getOxygenSaturation());
+    Bluetooth.print(delimitador);
+    Bluetooth.print(eHealth.getTemperature());
+    Bluetooth.print(delimitador); 
+    Bluetooth.print(eHealth.getSkinConductance());
+    Bluetooth.print(delimitador); 
+    Bluetooth.print(eHealth.getSkinResistance());
+    Bluetooth.print(finPaquete);
    }
 }
 
